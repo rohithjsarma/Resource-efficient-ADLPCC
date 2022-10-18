ca0041904034edc9a5923312d56042bfd77bb9b2
Rohith Jayakumara Sarma <rohith@dpr-gaming.seld.rnd.ericsson.se>
2022-10-18 07:54:45 +0200

    changed main code

--- src/ADLPCC.py
+++ src/ADLPCC.py
@@ -61,43 +61,68 @@ SCALES_LEVELS = 64
 # tf.config.optimizer.set_jit(True)
 # os.environ['TF_ENABLE_AUTO_MIXED_PRECISION'] = '1'
 
+# function to load data sequentially
+def load_data(path):
+    print(path)
+    filename = tf.strings.as_string(path)
+    print(filename)
+    data = np.load(filename) # read compressed npy
+    return data_tensor
 
-def train(args):
-    """Trains the model."""
+def _set_shapes(data):
+    data.set_shape((64,64,64,1))
+    return data
 
-    # Create input data pipeline
-    # Get the different models directory names
-    pc_names = glob.glob(args.train_data)
-
-    total_blocks = []
-    for i in range(len(pc_names)):
-        # Load input PC, get list of coordinates
-        in_points = pc2vox.load_pc(pc_names[i])
-        # Divide PC into blocks of the desired size. Get list of relative coordinates for points in each block
-        blocks, _ = pc2vox.pc2blocks(in_points, 64)
-        # Ignore blocks with fewer than 500 points
-        total_blocks.extend([blk for blk in blocks if len(blk) >= 500])
-
-    vox_data = np.zeros([len(total_blocks), 64, 64, 64, 1], dtype=np.float32)
-    # Iterate all blocks
-    for j in range(len(total_blocks)):
-        # Convert coordinates to 3D block
-        vox_data[j, :, :, :, :] = pc2vox.point2vox(total_blocks[j], 64)
-
-    # Shuffle all blocks
+def placeholder_func(vox_data, b_size):
     np.random.shuffle(vox_data)
-
     train_data_placeholder = tf.placeholder(vox_data.dtype, vox_data.shape)
-
     train_dataset = tf.data.Dataset.from_tensor_slices(train_data_placeholder)
-
     train_dataset = train_dataset.shuffle(buffer_size=len(vox_data)).repeat()
-    train_dataset = train_dataset.batch(args.batchsize)
+    train_dataset = train_dataset.batch(b_size)
     train_dataset = train_dataset.prefetch(16)
 
-    # Get training patch from dataset
-    x_data_iterator = train_dataset.make_initializable_iterator()
-    x = x_data_iterator.get_next()
+    return train_dataset, train_data_placeholder
+
+def read_npy_file(item):
+    data = np.load(item.decode())
+    data.set_shape((64,64,64,1))
+    print("************************************",data)
+    return data.astype(np.float32) 
+
+def tf_data_generator(d_path, file_list, i, batch_size = 4):
+    if i%(np.floor(len(file_list)/batch_size)-1) == 0:
+        if i == 0:
+            file_chunk = file_list[i*batch_size:(i+1)*batch_size]
+        else:
+            count = int(np.floor(len(file_list)/batch_size)-1)
+            file_chunk = file_list[count*batch_size:len(file_list)]
+            print("**************Epoch**************")
+            np.random.shuffle(file_list)
+
+    else:
+        count = int(i%(np.floor(len(file_list)/batch_size)))
+        file_chunk = file_list[count*batch_size:(count+1)*batch_size] 
+        
+    data = []
+    for file in file_chunk:
+        temp = np.load(os.path.join(d_path,file))
+        data.append(temp)
+
+    data = np.asarray(data).reshape(-1,64,64,64,1)
+    return data
+
+
+def train(args):
+
+    """Trains the model."""   
+    path = args.train_data
+    data_files = os.listdir(path)
+    np.random.shuffle(data_files)
+    print(len(data_files))
+    ite = 0
+
+    # each numpy array is of shape (64, 64, 64, 1)
+    train_data_placeholder = tf.placeholder('float32', [args.batchsize, 64, 64, 64, 1])
 
     # Instantiate model
     analysis_transform = AnalysisTransform(args.num_filters)
@@ -107,7 +132,7 @@ def train(args):
     entropy_bottleneck = tfc.EntropyBottleneck()
 
     # Build autoencoder and hyperprior
-    y = analysis_transform(x)
+    y = analysis_transform(train_data_placeholder)
     z = hyper_analysis_transform(abs(y))
     z_tilde, z_likelihoods = entropy_bottleneck(z, training=True)
     sigma = hyper_synthesis_transform(z_tilde)
@@ -117,10 +142,10 @@ def train(args):
     x_tilde = synthesis_transform(y_tilde)
 
     # Compute distortion: Focal Loss
-    train_mse = loss_functions.focal_loss(x, x_tilde, gamma=args.fl_gamma, alpha=args.fl_alpha)
+    train_mse = loss_functions.focal_loss(train_data_placeholder, x_tilde, gamma=args.fl_gamma, alpha=args.fl_alpha)
 
     # Compute rate: Total number of bits divided by number of points
-    num_input_points = tf.reduce_sum(x)
+    num_input_points = tf.reduce_sum(train_data_placeholder)
     train_bpp = (tf.reduce_sum(tf.log(y_likelihoods)) + tf.reduce_sum(tf.log(z_likelihoods))) / (
                 -np.log(2) * num_input_points)
 
@@ -150,22 +175,49 @@ def train(args):
     tf.summary.histogram("y_tilde", y_tilde)
     tf.summary.histogram("y", y)
 
-    sess_config = tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.8))
+    g_step = tf.train.get_or_create_global_step()
+    writer = tf.summary.FileWriter(args.checkpoint_dir)
+    summaries = tf.summary.merge_all()
 
-    def init_fn(scaffold, session):
-        session.run(x_data_iterator.initializer, feed_dict={train_data_placeholder: vox_data})
+    #sess_config = tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.5))
+    init_op = tf.global_variables_initializer()
 
-    hooks = [
-        tf.train.StopAtStepHook(last_step=args.last_step),
-        tf.train.NanTensorHook(train_loss),
-    ]
-    with tf.train.MonitoredTrainingSession(
-            hooks=hooks, checkpoint_dir=args.checkpoint_dir,
-            save_checkpoint_secs=3600, save_summaries_steps=1000,
-            scaffold=tf.train.Scaffold(init_fn=init_fn),
-            config=sess_config) as sess:
-        while not sess.should_stop():
-            sess.run(train_op)
+    if os.path.isfile(os.path.join(args.checkpoint_dir, 'model.ckpt.meta')):
+        with tf.Session() as sess:
+            #sess.run([writer.init(), step.initializer])
+            saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=1, save_relative_paths=True)
+            new_saver = tf.train.import_meta_graph(os.path.join(args.checkpoint_dir, 'model.ckpt.meta'))
+            new_saver.restore(sess, tf.train.latest_checkpoint(args.checkpoint_dir))
+            #sess.run(x_data_iterator.initializer)
+            sess.run(init_op)
+            stat_g_step = sess.run(g_step)
+            for i in range(stat_g_step, args.last_step):
+                x_data = tf_data_generator(path, data_files, i, args.batchsize)
+                feed_dict = {train_data_placeholder: x_data}
+                sess.run(train_op, feed_dict=feed_dict)
+                if i%1000==0:
+                    print("Iter: ", i)
+                    summ = sess.run(summaries, feed_dict=feed_dict)
+                    writer.add_summary(summ, global_step=i)
+                    saver.save(sess, os.path.join(args.checkpoint_dir,'model.ckpt'), write_meta_graph=True)    
+                
+    else:
+        saver = tf.train.Saver(max_to_keep=4, keep_checkpoint_every_n_hours=1, save_relative_paths=True)
+        with tf.Session() as sess:
+            sess.run(init_op)
+            #sess.run(x_data_iterator.initializer) 
+            for i in range(args.last_step):
+                x_data = tf_data_generator(path, data_files, i, args.batchsize)
+                feed_dict = {train_data_placeholder: x_data}
+                sess.run(train_op, feed_dict=feed_dict)
+                if i%1000==0:
+                    print("Iter: ", i)
+                    summ = sess.run(summaries, feed_dict=feed_dict )
+                    writer.add_summary(summ, global_step=i)
+                    saver.save(sess, os.path.join(args.checkpoint_dir,'model.ckpt'), write_meta_graph=True)
+                    
+                
+                           
 
 
 def compress(args):
@@ -210,11 +262,12 @@ def compress(args):
             raise ValueError("Input must be a PLY file (.ply extension).")
 
         pc_filename = os.path.splitext(os.path.basename(in_file))[0]
-        stream_dir = os.path.join("..", "results", os.path.split(os.path.split(args.checkpoint_dir)[0])[1], pc_filename)
+        stream_dir = os.path.join("/media/extdrive/rohith/", "results_dynamic_data_batch16", os.path.split(os.path.split(args.checkpoint_dir)[0])[1], pc_filename)
         os.makedirs(stream_dir, exist_ok=True)
 
         # Load input PC, get list of coordinates
         in_points = pc2vox.load_pc(in_file)
+        print(len(in_points))
         # Divide PC into blocks of the desired size. Get list of relative coordinates for points in each block
         blocks, blk_map = pc2vox.pc2blocks(in_points, args.blk_size)
 
@@ -267,9 +320,9 @@ def compress(args):
         os.remove(os.path.join(stream_dir, pc_filename + ".pkl"))
 
 
+
 def decompress(args):
     """Decompresses all test voxel_blocks."""
-
     # Read the shape information and compressed string from the binary file
     string = tf.placeholder(tf.string, [1])
     side_string = tf.placeholder(tf.string, [1])
@@ -299,9 +352,11 @@ def decompress(args):
 
     sess_config = tf.ConfigProto(gpu_options=tf.GPUOptions(per_process_gpu_memory_fraction=0.8))
 
+    print("*****************opening session***************")
     with tf.Session(config=sess_config) as sess:
         # Get the different models directory names
         model_names = glob.glob(args.checkpoint_dir)
+        print(model_names)
 
         # Manage input and output directories
         stream_filename = args.input_file
@@ -314,7 +369,8 @@ def decompress(args):
 
         with open(stream_filename + ".dec.pkl", "rb") as f:
             blk_size, best_model, blk_map, final_bitstream = pickle.load(f)
-
+        print(best_model)
+        
         try:
             # Initialize the reconstructed PC (empty)
             pts_geom = np.array([], dtype=np.int32).reshape(0, 3)
@@ -324,24 +380,32 @@ def decompress(args):
                 # Load the latest model checkpoint
                 latest = tf.train.latest_checkpoint(checkpoint_dir=model_names[j])
                 tf.train.Saver().restore(sess, save_path=latest)
+                
                 # Iterate all blocks
                 for i in range(len(best_model)):
                     if best_model[i] == j:
+                        print("1")
                         # Unpack string corresponding to the coded block
                         packed = tfc.PackedTensors(final_bitstream[i])
                         arrays = packed.unpack(tensors)
+                        
                         # Decode block
                         x_rec = sess.run(x_hat, feed_dict=dict(zip(tensors + [x_shape], arrays + [[blk_size, blk_size, blk_size]])))
+                        #print(x_rec)
                         # Convert back to point coordinates
                         points = pc2vox.vox2point(np.greater_equal(np.squeeze(x_rec), 0.5))
+                        print(points)
                         points = points + (blk_size * blk_map[i])
+                        print(points)
                         # Merge block points in the fully reconstructed PC
                         pts_geom = np.concatenate((pts_geom, points))
+                        print("5")
 
+            print(pts_geom)            
             # Write reconstructed PC to file
             pc2vox.save_pc(pts_geom, stream_filename + ".dec.ply")
             os.remove(stream_filename + ".dec.pkl")
-
+            print("7")
         except tf.errors.OutOfRangeError:
             pass
 
